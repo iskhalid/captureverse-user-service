6818092d434f15ea00a7c6100639b504a663a8aa
khaidprogramming <khalidmiya399@gmail.com>
2024-06-13 14:14:15 +0545

    setup project and create user db

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+
+const {PORT} = require('./config/serverConfig');
+// const CityRepository = require('./repository/city-repository')
+// const ApiRoutes = require('./routes/index');
+
+const setupAndStartServer = async () => {
+  //create the express object
+  const app = express();
+
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({extended: true}));
+
+//   app.use('/api',ApiRoutes);
+
+  app.listen(PORT, () => {
+    console.log(`Server started at ${PORT}`);
+
+  });
+};
+
+setupAndStartServer();
